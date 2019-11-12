69b4392fe1944ba2d1461186b2c66ecd920c67aa
weijx <wjx010107@163.com>
2019-11-12 17:10:56 +0800

    update_pwm

--- sf_f/engine/pwm_test.py
+++ sf_f/engine/pwm_test.py
@@ -18,34 +18,34 @@ import time
 pwm = Adafruit_PCA9685.PCA9685()
  
  
-def set_servo_angle(channel, angle):              
-    date=4096*((angle*11)+500)/20000 
-    pwm.set_pwm(channel, 0,int(date))
+def set_servo_angle(tchannel, tangle):              
+    date=4096*((tangle*11)+500)/20000 
+    pwm.set_pwm(tchannel, 0,int(date))
  
-pwm.set_pwm_freq(60)
- 
-angle=0 
-print('Moving servo on channel x, press Ctrl-C to quit...')
-while 1:
-    print(angle)
-    f=int(input('forward or back:'))
-    if f==1:
-        f_angle=int(input('pleas input f_angle:'))
-        for i in range(f_angle):
-            set_servo_angle(0, angle+i)
+def turn_servo(channel,direction,angle):
+	location=0
+    if direction==1:
+        for i in range(angle):
+            set_servo_angle(channel, location+i)
             time.sleep(0.01)
-        angle=f_angle+angle
-    elif f==0:
-        b_angle=int(input('pleas input b_angle:'))
-        if angle<b_angle:
-            for i in range(angle):
-                set_servo_angle(0,angle-i)
+        location=angle+location
+    elif direction==0:
+        if location<angle:
+            for i in range(location):
+                set_servo_angle(channel,location-i)
                 time.sleep(0.01)
-            angle=0
+            location=0
         else:
-            for i in range(b_angle):
-                set_servo_angle(0,angle-i)
+            for i in range(angle):
+                set_servo_angle(channel,location-i)
                 time.sleep(0.01)
-            angle=angle-b_angle
+            location=location-angle
     else:
         print('please input 1 or 0')
+
+
+pwm.set_pwm_freq(60)
+ 
+print('Moving servo on channel x, press Ctrl-C to quit...')
+while 1:
+    turn_servo(0,1,90)
