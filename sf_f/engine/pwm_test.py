7b52620f5e0ca8e873cf76386b6b87a6cdff4977
weijx <wjx010107@163.com>
2019-11-12 17:15:09 +0800

    update_pwm

--- sf_f/engine/pwm_test.py
+++ sf_f/engine/pwm_test.py
@@ -1,16 +1,9 @@
-#以下为python源程序
- 
-#输入通道与角度。即可选通并使该通道的舵机转动到相应的角度
-from __future__ import division								#导入 __future__ 文件的 division 功能函数(模块、变量名....)   #新的板库函数  //=
+from __future__ import division								
 import time
  
-# Import the PCA9685 module.
-import Adafruit_PCA9685										#导入Adafruit_PCA9685模块
- 
- 
-# Uncomment to enable debug output.
-#import logging
-#logging.basicConfig(level=logging.DEBUG)					#调试打印日志输出
+
+import Adafruit_PCA9685										
+
  
 import Adafruit_PCA9685
 import time
@@ -23,7 +16,7 @@ def set_servo_angle(tchannel, tangle):
     pwm.set_pwm(tchannel, 0,int(date))
  
 def turn_servo(channel,direction,angle):
-	location=0
+		location=0
     if direction==1:
         for i in range(angle):
             set_servo_angle(channel, location+i)
