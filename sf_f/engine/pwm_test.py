a575fd559c090fba3a0af899319dfb24b69e3787
weijx <wjx010107@163.com>
2019-11-12 15:52:32 +0800

    servo_forward_back

--- sf_f/engine/pwm_test.py
+++ sf_f/engine/pwm_test.py
@@ -12,29 +12,40 @@ import Adafruit_PCA9685										#导入Adafruit_PCA9685模块
 #import logging
 #logging.basicConfig(level=logging.DEBUG)					#调试打印日志输出
  
-# Initialise the PCA9685 using the default address (0x40).
-pwm = Adafruit_PCA9685.PCA9685()							#把Adafruit_PCA9685.PCA9685()引用地址赋给PWM标签
+import Adafruit_PCA9685
+import time
  
-# Alternatively specify a different address and/or bus:
-#pwm = Adafruit_PCA9685.PCA9685(address=0x41, busnum=2)
- 
- 
-#2^12精度  角度转换成数值  #angle输入的角度值(0--180)  #pulsewidth高电平占空时间(0.5ms--2.5ms)   #/1000将us转换为ms  #20ms时基脉冲(50HZ)
-#pulse_width=((angle*11)+500)/1000;  //将角度转化为500(0.5)<-->2480(2.5)的脉宽值(高电平时间)   angle=180时  pulse_width=2480us(2.5ms)
-#date/4096=pulse_width/20 ->有上pulse_width的计算结果得date=4096*( ((angle*11)+500)/1000 )/20   -->int date=4096((angle*11)+500)/20000;
-	 	 
-def set_servo_angle(channel, angle):					#输入角度转换成12^精度的数值
-	date=int(4096*((angle*11)+500)/(20000)+0.5)				#进行四舍五入运算 date=int(4096*((angle*11)+500)/(20000)+0.5)	
-	pwm.set_pwm(channel, 0, date)
+pwm = Adafruit_PCA9685.PCA9685()
  
  
-# Set frequency to 50hz, good for servos.
-pwm.set_pwm_freq(50)
+def set_servo_angle(channel, angle):              
+    date=4096*((angle*11)+500)/20000 
+    pwm.set_pwm(channel, 0,int(date))
  
+pwm.set_pwm_freq(60)
+ 
+angle=0 
 print('Moving servo on channel x, press Ctrl-C to quit...')
-while True:
-    	# Move servo on channel O between extremes.
-	channel=int(input('pleas input channel:'))
-	angle=int(input('pleas input angle:'))
-	set_servo_angle(channel, angle)
-	time.sleep(1)
+while 1:
+    print(angle)
+    f=int(input('forward or back:'))
+    if f==1:
+        f_angle=int(input('pleas input f_angle:'))
+        for i in range(f_angle):
+            set_servo_angle(0, angle+i)
+            time.sleep(0.01)
+        angle=f_angle+angle
+    elif f==0:
+        b_angle=int(input('pleas input b_angle:'))
+        if angle<b_angle:
+            for i in range(angle):
+                set_servo_angle(0,angle-i)
+                time.sleep(0.01)
+            angle=0
+        else:
+            for i in range(b_angle):
+                set_servo_angle(0,angle-i)
+                time.sleep(0.01)
+            angle=angle-b_angle
+    else:
+        print('please input 1 or 0')
