a6e946d2585e2a9dc2e53a22d24650f407c11a93
weijx <wjx010107@163.com>
2019-11-14 14:11:28 +0800

    'motorup'

--- sf_f/engine/motor.py
+++ sf_f/engine/motor.py
@@ -9,33 +9,33 @@ def set_servo_angle(channel, angle):
     pwm.set_pwm(channel, 0,int(date))
  
 pwm.set_pwm_freq(60)
-chanles=[[0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0]]
+channels=[[0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0]]
 print('Moving servo on channel x, press Ctrl-C to quit...')
 while 1:
-    print(chanles)
-    chanle=int(input('choice a chanle:'))
-    angle=chanles[chanle][-1]
+    print(channels)
+    channel=int(input('choice a channel:'))
+    angle=channels[channel][-1]
     f=int(input('forward or backward:'))
     if f==1:
         f_angle=int(input('pleas input forward_angle:'))
         for i in range(f_angle):
-            set_servo_angle(chanle, angle+i)
+            set_servo_angle(channel, angle+i)
             time.sleep(0.01)
         angle=f_angle+angle
-        chanles[chanle].append(angle)
+        channels[channel].append(angle)
     elif f==0:
         b_angle=int(input('pleas input backward_angle:'))
         if angle<b_angle:
             for i in range(angle):
-                set_servo_angle(chanle,angle-i)
+                set_servo_angle(channel,angle-i)
                 time.sleep(0.01)
             angle=0
-            chanles[chanle].append(angle)
+            channels[channel].append(angle)
         else:
             for i in range(b_angle):
-                set_servo_angle(chanle,angle-i)
+                set_servo_angle(channel,angle-i)
                 time.sleep(0.01)
             angle=angle-b_angle
-            chanles[chanle].append(angle)
+            channels[channel].append(angle)
     else:
         print('please input 1 or 0,1 is forward and 0 is backward')
