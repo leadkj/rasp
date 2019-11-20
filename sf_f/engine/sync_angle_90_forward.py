dc081d6d78d4a719cef695b5c4c503eca8741631
weijx <wjx010107@163.com>
2019-11-20 14:41:15 +0800

    'sync4'

--- sf_f/engine/sync_angle_90_forward.py
+++ sf_f/engine/sync_angle_90_forward.py
@@ -12,6 +12,8 @@ pwm.set_pwm_freq(60)
 print('Moving servo on channel x, press Ctrl-C to quit...')
 s1=0
 s2=0
+s3=0
+s4=0
 sleep=0
 while 1:
 
@@ -36,4 +38,27 @@ while 1:
         time.sleep(sleep)
     s1=s1-90
     s2=s2-90
+
+
+    if s3==0:
+        for i in range(91):
+            set_servo_angle(2,i)
+            set_servo_angle(3,i)
+            time.sleep(sleep)
+            s3=i
+            s4=i
+        print('if1')
+    else:
+        for i in range(91):
+            set_servo_angle(2,s1-i)
+            set_servo_angle(3,s2-i)
+            time.sleep(sleep)
+        s3=s3-90
+        s4=s4-90
+    for i in range(90):
+        set_servo_angle(2,s3-i)
+        set_servo_angle(3,s4-i)
+        time.sleep(sleep)
+    s3=s3-90
+    s4=s4-90
 
