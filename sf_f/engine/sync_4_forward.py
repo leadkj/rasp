dd9da866d39abe474de147e862f657500f0e7610
weijx <wjx010107@163.com>
2019-11-26 16:32:42 +0800

    '4forward'

--- sf_f/engine/sync_4_forward.py
+++ sf_f/engine/sync_4_forward.py
@@ -12,7 +12,8 @@ def set_servo_angle(channel, angle):
     pwm.set_pwm(channel, 0,int(date))
  
 pwm.set_pwm_freq(60)
-def setup():  ##舵机回到初始位置
+    
+def over():  ##舵机回到初始位置
     print(s1,s2,s3,s4)
     for i in range(s1):
         set_servo_angle(0,s1-i)
@@ -20,12 +21,24 @@ def setup():  ##舵机回到初始位置
     for i in range(s2):
         set_servo_angle(1,s2-i)
         time.sleep(sleep)
-    for i in range(s3):
-        set_servo_angle(2,s3-i)
-        time.sleep(sleep)
-    for i in range(s4):
-        set_servo_angle(3,s4-i)
-        time.sleep(sleep)
+    if s3>90:
+        for i in range(s3-90):
+            set_servo_angle(2,s3-i)
+            time.sleep(sleep)
+    elif s3<90:
+        print("s3<90")
+        for i in range(90-s3):
+            set_servo_angle(2,s3+i)
+            time.sleep(sleep)
+    if s4>90:
+        for i in range(s4-90):
+            set_servo_angle(3,s4-i)
+            time.sleep(sleep)
+    elif s4<90:
+        print("s4<90")
+        for i in range(90-s4):
+            set_servo_angle(3,s4+i)
+            time.sleep(sleep)
    
 print('Moving servo on channel x, press Ctrl-C to quit...')
 s1=0
@@ -56,47 +69,39 @@ try:
         
 
 
-        if s8==0:
+        if s3==0:
+            print('if2')
             for i in range(91):
-                set_servo_angle(7,i)
-                set_servo_angle(8,i)
+                set_servo_angle(2,91-i)
+                set_servo_angle(3,91-i)
                 time.sleep(sleep)
-                s8=i
-                s9=i
-                s10=i
-                s11=i
-                s12=i
-                s13=i
-                s14=i
+                s3=90-i
+                s4=90-i
         else:
-            print('else1')
+            print('else2')
             for i in range(91):
-                set_servo_angle(7,s1-i)
-                set_servo_angle(8,s2-i)
-              
+                set_servo_angle(2,91-i)
+                set_servo_angle(3,91-i)
                 time.sleep(sleep)
-            s8=s8-90
-            s9=s9-90
+                s3=91-i
+                s4=91-i
     
         for i in range(90):
             set_servo_angle(0,90-i)
             set_servo_angle(1,90-i)
-          
             time.sleep(sleep)
             s1=s1-1
             s2=s2-1
-           
         for i in range(90):
             print('for2')
-            set_servo_angle(7,90-i)
-            set_servo_angle(8,90-i)
-          
+            set_servo_angle(2,i)
+            set_servo_angle(3,i)
             time.sleep(sleep)
-            s8=s8-1
-            s9=s9-1
+            s3=i
+            s4=i
          
 
         
 except KeyboardInterrupt:
-    setup()
+    over()
     print("exit")
